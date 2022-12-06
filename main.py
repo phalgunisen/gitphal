f5f385ffd94ca3fbfcfed3c7a8e72b25d7b53d1f
phalguni <phalgunisen7@gmail.com>
2022-12-06 12:36:47 +0530

    my req

---
+++ main.py
@@ -0,0 +1,36 @@
+#
+# def sasuma(fuck):
+#     def wrapper(*args,**kwargs):
+#         fuck(*args,**kwargs)
+#         print('itd ok')
+#         print("but yiou have to puja krisha bhagawan")
+#     return wrapper
+# @sasuma
+# def mirra(a , b,*args):
+#     c = a+b
+#     print(c)
+#     print("maa mu chian mu jehjuis nku opuja kare")
+
+# def dipoo(func):
+#     def wrapper(*args, **kwargs):
+#         func(*args, **kwargs)
+#         print("mo maa raji hebe but tankara condition achi")
+#     return wrapper
+#
+# @dipoo
+class Puchuu:
+    height = 12
+
+
+    def puchu(a,b,*args):
+        c = a+b
+        print(c)
+        print("tame maa raji hebe")
+
+class Dipoo:
+    def  know(self):
+        print('pytghon')
+
+
+
+
